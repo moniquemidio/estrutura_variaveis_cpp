316b2894149a2a58c4fd70f9f6ac1de64e65508f
Monique Emídio de Oliveira <49343419+moniquemidio@users.noreply.github.com>
2025-11-03 16:36:38 -0300

    tipos de variáveis

---
+++ tipos_variaveis.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int quantidade = 10;
+    double preco = 5.99;
+    char letra = 'A';
+    bool ativo = true;
+    string nome= "Mona";
+    
+    cout << "A quantidade e: " << quantidade << endl;
+    cout << "O preco e: " << preco << endl;
+    cout << "A alternativa e: " << letra << endl;
+    cout << "O verdadeiro e: " << ativo << endl;
+    cout << "Meu nome e: " << nome;
+
+    return 0;
+}
