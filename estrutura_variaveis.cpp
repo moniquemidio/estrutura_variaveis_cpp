7fd7e7a484e181bdcc6c055a4fe921ec63bd869b
Monique Emídio de Oliveira <49343419+moniquemidio@users.noreply.github.com>
2025-11-03 16:35:27 -0300

    estrutura_variaveis

---
+++ estrutura_variaveis.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string nome = "Monique";
+    int idade = 25;
+    
+    cout << "A " << nome << " tem " << idade << " anos." <<endl;
+
+    return 0;
+}
